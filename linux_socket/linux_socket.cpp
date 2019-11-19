1a7130a812fbae9299331aeff51bf388a74ff962
JackChanceLi <1769264507@qq.com>
2019-11-19 15:03:47 +0800

    ADD: 通过socket实现tcp server

---
+++ linux_socket/linux_socket.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h> // for close()
+#include <string.h> // for bzero()
+#define PORT 8881
+#define MESSAGE_LEN 1024
+int main(int argc, char *argv[])
+{
+    int ret = -1;
+    int on = 1;
+    int socket_fd, accept_fd;
+    int backlog = 10; //缓冲长度，与并发量相关
+    struct sockaddr_in localaddr, remoteaddr;
+
+    char in_buff[MESSAGE_LEN] = {
+        0,
+    }; // 接收缓冲区
+
+    socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
+    {
+        std::cout << "failed to create socket!" << std::endl;
+        exit(-1);
+    }
+    /* Set socket FD's option OPTNAME at protocol level LEVEL
+   to *OPTVAL (which is OPTLEN bytes long).
+   Returns 0 on success, -1 for errors.  */
+    ret = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR,
+                     &on, sizeof(on));
+    if (ret == -1)
+    {
+        std::cout << "failed to set socket options!" << std::endl;
+    }
+    localaddr.sin_family = AF_INET;
+    localaddr.sin_port = PORT;
+    localaddr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(localaddr.sin_zero), 8);
+    ret = bind(socket_fd, (struct sockaddr *)&localaddr, sizeof(struct sockaddr));
+    if (ret == -1)
+    {
+        std::cout << "failed to bind " << std::endl;
+        exit(-1);
+    }
+    ret = listen(socket_fd, backlog);
+    if (ret == -1)
+    {
+        std::cout << "failed to listen " << std::endl;
+        exit(-1);
+    }
+    for (;;)
+    {
+        socklen_t addr_len = sizeof(struct sockaddr);
+        accept_fd = accept(socket_fd,
+                           (struct sockaddr *)&remoteaddr,
+                           &addr_len);
+        int pid = fork();
+        if (pid == 0)
+        {
+            for (;;)
+            {
+                ret = recv(accept_fd, (void *)in_buff, MESSAGE_LEN, 0);
+                if (ret == 0) // 说明没数据了
+                {
+                    std::cout << "recv finish，end！ " << std::endl;
+                    break;
+                }
+                std::cout << "receive:" << in_buff << std::endl;
+                // 返回客户端
+                send(accept_fd, (void *)in_buff, MESSAGE_LEN, 0);
+            }
+            close(accept_fd);
+        }
+    }
+    close(socket_fd);
+    return 0;
+}
---
+++ note/linux_socket.md
@@ -0,0 +1,26 @@
+# Socket 编程基础
+
+## 实现一个TCP Server
+#### Server端步骤
+- socker():创建socket，指定使用TCP协议
+- bind():将socket与地址和端口进行绑定
+- listen():侦听端口
+- accept():创建新的socket
+- recv():使用recv接收数据
+- send():使用send发送数据
+- close():使用close关闭连接
+#### Client端步骤
+- socket():创建socket，指定使用TCP协议
+- connect():操作系统随机分配一个随机的端口和IP地址
+- send():发送
+- recv():接收
+- close():关闭
+#### TCP常见套接字选项
+- SO_REUSEADDR: 地址重用
+    - 之前程序结束，端口处于WAIT_TIME状态下，新启动的程序仍然可以启动
+- SO——RCVBUF：设置接收缓冲区大小
+- SO_SNDBUF: 设置发送缓冲区大小
+#### Demo
+```c++
+
+```
